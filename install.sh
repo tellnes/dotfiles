bf56cf83ad808420c3c44b48dc32941c80b2a81e
Christian Tellnes <christian@tellnes.no>
2012-03-12 19:10:34 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+BASE="$(cd "$(dirname "$0")" && pwd)"
+
+
+# Very simple install script
+
+for file in `find ${BASE} -maxdepth 1 -not -name '.*' -not -name install.sh -not -name bin -not -name etc -not -name README.txt -not -name LICENSE -type f`; do
+    filename=`basename "$file"`
+    echo "ln -sf $BASE/$filename -> ~/.$filename"
+    ln -sf "$BASE/$filename" ~/.$filename
+done
+
+mkdir -p ~/bin/
+for file in "$BASE"/bin/*; do
+    filename=`basename "$file"`
+    echo "ln -sf $BASE/$filename -> ~/bin/$filename"
+    ln -sf "$BASE/bin/$filename" ~/bin/$filename
+done
+
